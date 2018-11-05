ae3cf85cf69da244bfbbba1f5e2b0cd002d027ed
palemre <palandoken.e@gmail.com>
2018-11-05 18:00:42 +0100

    :sparkles: Add shoot target

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const shooter = {}
  shooter.$score = shooter.$container.querySelector('.score .value')
  shooter.$timer = shooter.$container.querySelector('.timer')
  shooter.$targets = shooter.$container.querySelector('.targets')
+ shooter.score = 0
 
 
  /***
@@ -23,6 +24,26 @@ const shooter = {}
      $target.style.top = `${Math.random() * 100}%`
      $target.style.left = `${Math.random() * 100}%`
      shooter.$targets.appendChild($target)
+
+     //Listen to mouse enter
+     $target.addEventListener('mouseenter', () =>
+     {
+        shooter.shootTarget($target)
+     })
+ }
+
+ shooter.shootTarget = (_$target) => //_ car c'est un paramètre
+ {
+    //Delete target
+    _$target.remove()
+
+    //Add new target
+    shooter.addTarget()
+
+    //Increment score
+    shooter.score++
+    shooter.$score.textContent = shooter.score
+    //Play sound
  }
 
  shooter.addTarget()
